35bc174a5e85265a67c18680c3af9a04381c3388
Michael Healy <m@michaelhealy.co>
2021-12-16 10:18:43 +0800

    Small typo

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+// Copyright 2019-2021 @polkadot/extension authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const base = require('@polkadot/dev/config/eslint.cjs');
+
+module.exports = {
+  ...base,
+  ignorePatterns: [
+    ...base.ignorePatterns,
+    "i18next-scanner.config.js"
+  ],
+  parserOptions: {
+    ...base.parserOptions,
+    project: [
+      './tsconfig.eslint.json'
+    ]
+  },
+  rules: {
+    ...base.rules,
+    // this seems very broken atm, false positives
+    '@typescript-eslint/unbound-method': 'off'
+  }
+};
